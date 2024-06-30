7b1cec2269d00afb659228287bfeb6154a229aa1
Sam Lanning <sam@samlanning.com>
2024-06-30 15:54:21 +0100

    run formatter

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import { diffJson } from '@arcanejs/diff';
+import { diffJson } from "@arcanejs/diff";
 
 export default function Home() {
   return (
