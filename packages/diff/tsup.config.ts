5e45a510d453563faddd0bb51645a94e9c25aa20
Sam Lanning <sam@samlanning.com>
2024-10-09 23:11:41 +0100

    Export sourcemaps for library types (#31)
    This will ease in development by allowing for the
    typescript language server (and IDEs / editors) to
    have jump-to-definition for the original source
    files for cross-package usage.
    
    We should be able to avoid non-existent JS modules
    from being importable by virtue of having exports
    specified in the package.json files.

--- packages/diff/tsup.config.ts
+++ packages/diff/tsup.config.ts
@@ -3,5 +3,4 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts", "src/diff.ts", "src/patch.ts"],
   format: ["cjs", "esm"],
-  dts: true,
 });
--- packages/toolkit/tsup.config.ts
+++ packages/toolkit/tsup.config.ts
@@ -3,5 +3,4 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["cjs", "esm"],
-  dts: true,
 });
