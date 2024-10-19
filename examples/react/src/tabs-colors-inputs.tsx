7a735358f3b6cbf06b1dc03cb85f74c711101177
Sam Lanning <sam@samlanning.com>
2024-10-19 15:38:59 +0100

    Introduce example usage of the react library

---
+++ examples/react/src/tabs-colors-inputs.tsx
@@ -0,0 +1,64 @@
+import * as React from 'react';
+import { Toolkit } from '@arcanejs/toolkit';
+
+import {
+  ToolkitRenderer,
+  Group,
+  Button,
+  Tabs,
+  Tab,
+  Switch,
+  Rect,
+  Label,
+  TextInput,
+} from '@arcanejs/react-toolkit';
+
+const toolkit = new Toolkit();
+
+toolkit.start({
+  mode: 'automatic',
+  port: 1333,
+});
+
+const App = () => {
+  const [switchState, setSwitchState] = React.useState<'off' | 'on'>('off');
+  const [color, setColor] = React.useState('purple');
+  const [textValue, setTextValue] = React.useState('initial text');
+
+  return (
+    <Tabs>
+      <Tab name="FooBar">
+        <Group>
+          {`Switch State: ${switchState}`}
+          <Switch
+            state={switchState}
+            onChange={(value) => setSwitchState(value)}
+          />
+          <Switch
+            state={switchState === 'on' ? 'off' : 'on'}
+            onChange={(value) => setSwitchState(value === 'on' ? 'off' : 'on')}
+          />
+        </Group>
+      </Tab>
+      <Tab name="Colors">
+        <Group>
+          <Rect color={color} />
+          <Button text="red" onClick={() => setColor('red')} />
+          <Button text="blue" onClick={() => setColor('blue')} />
+          <Button
+            text="translucent green"
+            onClick={() => setColor('rgba(0, 255, 0, 0.5)')}
+          />
+        </Group>
+      </Tab>
+      <Tab name="Another">
+        <Group>
+          <Label text={textValue} />
+          <TextInput value={textValue} onChange={setTextValue} />
+        </Group>
+      </Tab>
+    </Tabs>
+  );
+};
+
+ToolkitRenderer.render(<App />, toolkit);
