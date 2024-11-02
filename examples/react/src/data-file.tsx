3a31c7527a0cd700beb0ecc0162cbb908556273d
Sam Lanning <sam@samlanning.com>
2024-11-02 17:51:07 +0000

    Add additional data file example

---
+++ examples/react/src/data-file.tsx
@@ -0,0 +1,85 @@
+import React, { useRef } from 'react';
+import { Toolkit } from '@arcanejs/toolkit';
+
+import type { TextInput as TextInputType } from '@arcanejs/toolkit';
+import {
+  ToolkitRenderer,
+  Group,
+  TextInput,
+  Button,
+} from '@arcanejs/react-toolkit';
+import { createDataFileSpec, useDataFile } from '@arcanejs/react-toolkit/data';
+import { z } from 'zod';
+import path from 'path';
+
+const DATA_DIR = path.join(path.dirname(__dirname), 'data', 'data-file');
+
+const toolkit = new Toolkit();
+
+toolkit.start({
+  mode: 'automatic',
+  port: 1336,
+});
+
+const DataSpec = createDataFileSpec({
+  schema: z.object({
+    text: z.string(),
+  }),
+  defaultValue: {
+    text: 'Hello World',
+  },
+  onPathChange: 'transfer',
+});
+
+const NAME_REGEX = /^[a-zA-Z0-9_-]+\.json$/;
+
+const validateName = (name: string) => {
+  if (!NAME_REGEX.exec(name)) {
+    throw new Error('Invalid filename for JSON file');
+  }
+};
+
+const FileDetails = () => {
+  const { data, updateData } = useDataFile(DataSpec);
+  return (
+    <Group direction="vertical">
+      <Group>
+        Data:
+        <TextInput
+          value={data.text}
+          onChange={(text) => updateData((current) => ({ ...current, text }))}
+        />
+      </Group>
+    </Group>
+  );
+};
+
+const App = () => {
+  const fileInputRef = useRef<TextInputType | null>(null);
+  const [filename, setFilename] = React.useState<string | null>(null);
+
+  return (
+    <Group direction="vertical">
+      Enter a filename to save data to, it must end in .json:
+      <Group>
+        {`${DATA_DIR}/`}
+        <TextInput ref={fileInputRef} />
+        <Button
+          text={'Use File'}
+          onClick={() => {
+            const name = fileInputRef.current?.getValue() ?? '';
+            validateName(name);
+            setFilename(name);
+          }}
+        />
+      </Group>
+      {filename && (
+        <DataSpec.Provider path={path.join(DATA_DIR, filename)}>
+          <FileDetails />
+        </DataSpec.Provider>
+      )}
+    </Group>
+  );
+};
+
+ToolkitRenderer.render(<App />, toolkit);
